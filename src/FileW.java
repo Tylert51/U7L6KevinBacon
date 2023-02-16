59e70accab242a35cabe4909b7e3405d9c01a51a
tyler <ttang7910@bths.edu>
2023-02-16 10:52:50 -0500

    making a list of all the actors and the movies they starred in

--- src/FileW.java
+++ src/FileW.java
@@ -1,3 +1,4 @@
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.io.File;
 import java.io.IOException;
@@ -7,24 +8,29 @@ import java.util.Collections;
 public class FileW {
     public static void main(String[] args) {
 
+        ArrayList<String> allActors = MovieDatabaseBuilder.getActorDB("src/all_actors.txt");
         ArrayList<SimpleMovie> movies = MovieDatabaseBuilder.getMovieDB("src/movies_sorted_delimiter.txt");
-        ArrayList<String> allActors = new ArrayList<String>();
+        ArrayList<Actor> actorMovies = new ArrayList<Actor>();
 
-        for(SimpleMovie m : movies) {
-            ArrayList<String> actors = m.getActors();
-            for(String str : actors) {
-                if(!allActors.contains(str)) {
-                    allActors.add(str);
+
+        for(String actor : allActors) {
+
+            ArrayList<SimpleMovie> moviesStarred = new ArrayList<SimpleMovie>();
+
+            for(SimpleMovie m : movies) {
+                if(m.getActors().contains(actor)) {
+                    moviesStarred.add(m);
                 }
             }
+            actorMovies.add(new Actor(actor, moviesStarred));
         }
 
         try {
-            File f = new File("src/all_actors.txt");
+            File f = new File("src/all_actors_movies.txt");
             f.createNewFile();
             FileWriter fw = new FileWriter(f);
-            for (String m : allActors) {
-                fw.write(m + "\n");
+            for (Actor a : actorMovies) {
+                fw.write(a + "\n");
             }
             fw.close();
         }
